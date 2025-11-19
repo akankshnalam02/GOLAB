a18093313305f18ebee3ae7d275034595d9d2838
akankshnalam02 <157952040+akankshnalam02@users.noreply.github.com>
2025-11-19 22:16:32 +0530

    KNN classifier

---
+++ ex8a.go
@@ -0,0 +1,23 @@
+package main
+import (
+	"fmt"
+	"log"
+	"math"
+	"github.com/sjwhitworth/golearn/base"
+	"github.com/sjwhitworth/golearn/evaluation"
+	"github.com/sjwhitworth/golearn/knn"
+)
+func main() {
+	irisData, err := base.ParseCSVToInstances("iris_2.csv", true)
+	if err != nil {
+		log.Fatal(err)
+	}
+	knn := knn.NewKnnClassifier("euclidean", "linear", 2)
+	cv, err := evaluation.GenerateCrossFoldValidationConfusionMatrices(irisData, knn, 5)
+	if err != nil {
+		log.Fatal(err)
+	}
+	mean, variance := evaluation.GetCrossValidatedMetric(cv, evaluation.GetAccuracy)
+	stdev := math.Sqrt(variance)
+	fmt.Printf("\nAccuracy\n%.2f (+/- %.2f)\n\n", mean, stdev*2)
+}
